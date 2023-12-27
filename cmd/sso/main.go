72abd544369475eeaed5a320ac0ecc31fab8aecc
IskanderSh <is2367361@gmail.com>
2023-12-27 11:45:53 +0300

    create config, main and other files

---
+++ cmd/sso/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"github.com/IskanderSh/sso/internal/app"
+	"github.com/IskanderSh/sso/internal/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/IskanderSh/sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MuslLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application", slog.Any("config", cfg))
+
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+
+	go application.GRPCServer.MustRun()
+
+	// TODO: запустить gRPC-сервер приложения
+
+	// Graceful shutdown
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	sign := <-stop
+	log.Info("stopping application", slog.String("signal", sign.String()))
+	application.GRPCServer.Stop()
+
+	log.Info("application stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog(slog.LevelDebug)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
+
+func setupPrettySlog(level slog.Level) *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: level,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
---
+++ internal/app/app.go
@@ -0,0 +1,23 @@
+package app
+
+import (
+	grpcapp "github.com/IskanderSh/sso/internal/app/grpc"
+	"log/slog"
+	"time"
+)
+
+type App struct {
+	GRPCServer *grpcapp.App
+}
+
+func New(
+	log *slog.Logger,
+	grpcPort int,
+	storagePath string,
+	tokenTTL time.Duration,
+) *App {
+	grpcApp := grpcapp.New(log, grpcPort)
+	return &App{
+		GRPCServer: grpcApp,
+	}
+}
